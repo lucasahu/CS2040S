99402690725fbdf7dda73ada491770ef42b8a56a
lucas <lucasignahumada@gmail.com>
2023-09-03 09:45:14 -0300

    Solve addingtrouble challenge

--- PS0/addingtrouble.java
+++ PS0/addingtrouble.java
@@ -1,5 +1,17 @@
 package PS0;
 
 public class addingtrouble {
-    
+    static void checkAdd(int a, int b, int c) {
+        if (a + b == c) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAdd(2, 3, 5);
+        checkAdd(1, 1, 3);
+        checkAdd(-1, 1, 0);
+    }
 }
