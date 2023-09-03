b462f683e87ced290eff1d24b1a5a491c8b74372
lucas <lucasignahumada@gmail.com>
2023-09-03 09:28:20 -0300

    Solve metronome challenge

--- PS0/metronome.java
+++ PS0/metronome.java
@@ -1,5 +1,13 @@
 package PS0;
 
 public class metronome {
-    
+    static void revolutions(double ticks) {
+        double result = ticks / 4;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        revolutions(16);
+        revolutions(99);
+    }
 }
